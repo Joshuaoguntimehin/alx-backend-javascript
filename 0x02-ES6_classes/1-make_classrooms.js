e88d3d2d2bc840eeab9be7aedd3f6a22d7b6a8e1
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-05 00:24:51 +0100

    alx tasks

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom.js'
+
+export defoult function initializeRoom(){
+	const room1 = new classRoom(19);
+	const room2 = new classRoom(20);
+	const  room3 = new classRoom(34);
+	return [room1, room2,]
+}
