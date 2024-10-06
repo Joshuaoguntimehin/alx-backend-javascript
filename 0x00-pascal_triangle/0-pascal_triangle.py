b19ac21544195d0d0ab14179e7c69da7553f7051
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-07 00:48:30 +0100

    alx tasks

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,11 +7,9 @@ def pascal_triangle(n):
     for i in range(n):
         row = [1] * (i + 1)  # Start with a row of 1s
         for j in range(1, i):
-            row[j] = triangle[i-1][j-1] + triangle[i-1][j]  # Sum of two elements above
-        triangle.append(row)
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
     
     return triangle
 
-# Example usage:
-print(pascal_triangle(5))
+
 
