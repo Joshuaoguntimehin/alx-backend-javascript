e3276ca3d8ae32dce894db5576c9cbd1782c319b
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-07 01:16:27 +0100

    alx tasks

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,10 +5,12 @@ def pascal_triangle(n):
         return []
     
     triangle = []
-    
+    """function in duc"""
     for i in range(n):
+        """function in duc"""
         row = [1] * (i + 1)  # Start with a row of 1s
         for j in range(1, i):
+            """function in duc"""
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]  # Sum of two elements above
         triangle.append(row)
     
