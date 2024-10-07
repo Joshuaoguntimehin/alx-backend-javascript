4d18ebf95cccdeab689d741c5b53290042984ef7
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-07 02:01:52 +0100

    alx tasks

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""function in duc"""
 def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's triangle of size n."""
     if n <= 0:
         return []
-    
-    triangle = []
-    """function in duc"""
-    for i in range(n):
-        """function in duc"""
-        row = [1] * (i + 1)  # Start with a row of 1s
+
+    triangle = [[1]]  # First row
+
+    for i in range(1, n):
+        row = [1]  # Start each row with a 1
+        # Calculate the values in between based on the previous row
         for j in range(1, i):
-            """function in duc"""
-            row[j] = triangle[i-1][j-1] + triangle[i-1][j]  # Sum of two elements above
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # End each row with a 1
         triangle.append(row)
-    
+
     return triangle
 
 
