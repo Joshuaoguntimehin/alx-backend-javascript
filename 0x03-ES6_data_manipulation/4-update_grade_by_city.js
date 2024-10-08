9d5bae2012aef94176f962be97596ae482767ade
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-08 19:06:19 +0100

    alx tasks

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    return students
-      .filter(student => student.location === city) // Filter students by city
-      .map(student => {
-        const gradeUpdate = newGrades.find(grade => grade.studentId === student.id);
-        // If a matching grade is found, update it; otherwise, keep the original grade
-        return {
-          ...student,
-          grade: gradeUpdate ? gradeUpdate.grade : student.grade || 'N/A'
-        };
-      });
-    }
+  return students
+    .filter((student) => student.location === city) // Filter students by city
+    .map((student) => {
+      const gradeUpdate = newGrades.find((grade) => grade.studentId === student.id);
+      // If a matching grade is found, update it; otherwise, keep the original grade
+      return {
+        ...student,
+        grade: gradeUpdate ? gradeUpdate.grade : student.grade || 'N/A',
+      };
+    });
+}
