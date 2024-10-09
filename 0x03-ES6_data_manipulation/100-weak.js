19a6174f151a511fabf48789f6749428296ce301
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-09 02:52:03 +0100

    alx tasks

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,15 +3,15 @@ export const weakMap = new WeakMap();
 
 // Exporting the queryAPI function
 export function queryAPI(endpoint) {
-    if (!weakMap.has(endpoint)) {
-        weakMap.set(endpoint, 0);
-    }
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
 
-    let queryCount = weakMap.get(endpoint);
-    queryCount += 1;
-    weakMap.set(endpoint, queryCount);
+  let queryCount = weakMap.get(endpoint);
+  queryCount += 1;
+  weakMap.set(endpoint, queryCount);
 
-    if (queryCount >= 5) {
-        throw new Error('Endpoint load is high');
-    }
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
