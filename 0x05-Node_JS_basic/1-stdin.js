4aad67e28da9a586c2d0ceee632e223eac8af1fa
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-26 13:46:21 +0100

    alx tasks

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,24 @@
+// 1-stdin.js
+
+// Import the 'readline' module to handle user input from the command line
 const readline = require('readline');
 
+// Create an interface for reading from and writing to the command line
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-function displayMessage() {
-    rl.question("Welcome to Holberton School, what is your name?:\n", (name) => {
-        console.log("Your name is:, " + name + "!");
-        rl.close(); // Close the input stream
-    });
-}
-// Call the function
-displayMessage();
+// Display the welcome message and prompt the user for their name
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  // Display the user's name
+  console.log(`Your name is: ${name}`);
+  
+  // Once the user provides their name, display the closing message and close the interface
+  rl.on('close', () => {
+    console.log('This important software is now closing');
+  });
+  
+  // Close the readline interface after the user input is processed
+  rl.close();
+});
