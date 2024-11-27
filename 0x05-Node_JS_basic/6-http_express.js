e04467730c299ba4ec6e146dd08e6a8303dc8c41
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-27 05:51:02 +0100

    alx tasks

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.write('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 module.exports = app;
