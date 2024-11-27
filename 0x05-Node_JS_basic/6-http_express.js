834e48b6941a4d29f933cb1fb710445e3b361765
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-27 05:46:12 +0100

    alx tasks

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) =>{
+    res.write('Hello Holberton School!')
+})
+
+module.exports = app;
+
+if (require.main === module) {
+    const PORT = 1245; // Define the port
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  }
+  
