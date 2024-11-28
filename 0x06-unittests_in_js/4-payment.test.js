045d1cffa06cab8cf10d8c946a84aa49a18db0b5
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-28 17:36:33 +0100

    alx tasks

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,24 @@
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should stub Utils.calculateNumber to always return 10 and verify console.log output', () => {
+    // Stub the Utils.calculateNumber function to always return 10
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+    // Spy on console.log
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    // Call the function to test
+    sendPaymentRequestToApi(100, 20);
+
+    // Assertions
+    sinon.assert.calledOnceWithExactly(calculateNumberStub, 'SUM', 100, 20); // Verify stub was called with the correct arguments
+    sinon.assert.calledOnceWithExactly(consoleLogSpy, 'The total is: 10'); // Verify console.log output
+
+    // Restore the stub and spy
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+});
