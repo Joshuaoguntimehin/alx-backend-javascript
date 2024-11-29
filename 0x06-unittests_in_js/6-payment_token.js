deb25a50442cadc871f42f102e30518028bfb200
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-29 02:52:16 +0100

    alx tasks

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -7,3 +7,5 @@ function getPaymentTokenFromAPI(success){
         }
     });
 }
+
+module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,17 +1,17 @@
-// Call done when the test has finished successfully
-const getPaymentTokenFromAPI = require('./6-payment_token.js'); // Adjust path as necessary
+// 6-payment_token.test.js
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-    it('should resolve with the correct data when called with true', (done) => {
+    test('should return a resolved promise with the correct data when success is true', (done) => {
         getPaymentTokenFromAPI(true).then((response) => {
             expect(response).toEqual({ data: 'Successful response from the API' });
-            done(); // Call done when the test has finished successfully
+            done();
         });
     });
 
-    it('should do nothing when called with false', (done) => {
-        const response = getPaymentTokenFromAPI(false);
-        expect(response).toBeUndefined();
-        done(); // Call done immediately as there's no async operation for the `false` case
+    test('should not resolve or reject when success is false', () => {
+        const promise = getPaymentTokenFromAPI(false);
+        expect(promise).toBeInstanceOf(Promise);
+        return expect(promise).resolves.toBeUndefined();
     });
 });
